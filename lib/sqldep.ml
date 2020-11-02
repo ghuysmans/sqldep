8bce9969154cc692b755ac0fac04b249b215fab2
Guillaume Huysmans <ghuysmans99@gmail.com>
2020-11-02 03:38:05 +0100

    Add unit tests

--- lib/sqldep.ml
+++ lib/sqldep.ml
@@ -21,3 +21,22 @@ let tables s =
   | Select sf -> None, aux sf
   | Create (name, `Select sf) -> Some name, aux sf
   | _ -> raise (Invalid_argument "tables")
+
+
+let%test_module _ = (module struct
+  let t sql =
+    Sqlgg.Parser.parse_stmt sql |>
+    tables |>
+    snd |>
+    List.map Sqlgg.Sql.show_table_name |>
+    List.sort compare
+
+  let%test "from_prod" = t "SELECT * FROM t, u" = ["t"; "u"]
+  let%test "join" = t "SELECT * FROM t INNER JOIN u ON t.x = u.x" = ["t"; "u"]
+  let%test "on_ex" = t "SELECT * FROM t INNER JOIN u ON EXISTS(SELECT * FROM v)" = ["t"; "u"; "v"]
+  let%test "where_eq" = t "SELECT * FROM t WHERE x = (SELECT * FROM u)" = ["t"; "u"]
+  let%test "where_in" = t "SELECT * FROM t WHERE x IN (SELECT * FROM u)" = ["t"; "u"]
+  let%test "group_by_ex" = t "SELECT * FROM t GROUP BY EXISTS(SELECT * FROM u)" = ["t"; "u"]
+  let%test "having_ex" = t "SELECT * FROM t GROUP BY x HAVING EXISTS(SELECT * FROM u)" = ["t"; "u"]
+  let%test "order_by_ex" = t "SELECT * FROM t ORDER BY EXISTS(SELECT * FROM u)" = ["t"; "u"]
+end)
