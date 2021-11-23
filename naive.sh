fe6f35142d668d0940f7cd4391c016a6afa1a0c0
Guillaume Huysmans <ghuysmans99@gmail.com>
2021-11-23 16:04:37 +0100

    Remove CR

--- naive.sh
+++ naive.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #please put each FROM/JOIN on a separate line
-#this won't work on files with DOS line endings
+tr -d '\r' |
 sed -nEe 's/CREATE VIEW ([A-Za-z0-9_.]+) AS/%!\1:/p' \
     -e 's/.*(FROM|JOIN) ([A-Za-z0-9_.]+(,\s*[A-Za-z0-9_.]+)*)/%\2\n/p' |
 sed -ne 's/^%\(.*\)/\1/p' |
