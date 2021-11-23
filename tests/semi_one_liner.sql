c7b6d2af3947f1091933176d22ff637f15e0a378
Guillaume Huysmans <ghuysmans99@gmail.com>
2021-11-23 19:28:22 +0100

    Almost handle one-liners

---
+++ tests/semi_one_liner.sql
@@ -0,0 +1,2 @@
+CREATE VIEW v AS
+SELECT * FROM t INNER JOIN u ON t.x=u.x
--- utils/naive.sh
+++ utils/naive.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-#please put each FROM/JOIN on a separate line
+#please put table enumerations on the same line (e.g. FROM t, u)
 tr -d '\r' |
+sed -E 's/(^|\s)(FROM|JOIN)\s+/\n\2 /g' |
 sed -nEe 's/CREATE VIEW ([A-Za-z0-9_.]+) AS/%!\1:/p' \
     -e 's/.*(FROM|JOIN)\s+([A-Za-z0-9_.]+(,\s+[A-Za-z0-9_.]+)*)/%\2\n/p' |
 sed -ne 's/^%\(.*\)/\1/p' |
