6e85d0d7f89ba1614862b1c43fc626b6ebe41382
Guillaume Huysmans <ghuysmans99@gmail.com>
2021-11-23 19:27:50 +0100

    Better handling of whitespaces

--- utils/naive.sh
+++ utils/naive.sh
@@ -2,7 +2,7 @@
 #please put each FROM/JOIN on a separate line
 tr -d '\r' |
 sed -nEe 's/CREATE VIEW ([A-Za-z0-9_.]+) AS/%!\1:/p' \
-    -e 's/.*(FROM|JOIN) ([A-Za-z0-9_.]+(,\s*[A-Za-z0-9_.]+)*)/%\2\n/p' |
+    -e 's/.*(FROM|JOIN)\s+([A-Za-z0-9_.]+(,\s+[A-Za-z0-9_.]+)*)/%\2\n/p' |
 sed -ne 's/^%\(.*\)/\1/p' |
 tr '\n!' ' \n' |
 tail -n +2
