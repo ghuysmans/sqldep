e3ae74c66d6deff6439a87bd1bd0e63b10034273
Guillaume Huysmans <ghuysmans99@gmail.com>
2021-11-27 18:34:53 +0100

    Fix: don't prefix twice

--- utils/dep_to_dot.sh
+++ utils/dep_to_dot.sh
@@ -4,8 +4,9 @@ echo 'digraph {'
 	if [ -z "$1" ]; then
 		cat
 	else
-		sed -Ee "s/([A-Za-z0-9_.]+)/$1.\1/g" \
-        -e 's/^[^.]+\.(TABLE|VIEW)/\1/'
+		sed -E "s/([A-Za-z0-9_.]+)/$1.\1/g" |
+		sed -E "s/$1\.([A-Za-z0-9_]+)\.([A-Za-z0-9_]+)/\1.\2/g" |
+		sed -E 's/^[^.]+\.(TABLE|VIEW)/\1/'
 	fi
 ) |
 sed -Ee 's/^TABLE ([^:]+)/\1 [shape=cylinder]\n\1/' \
