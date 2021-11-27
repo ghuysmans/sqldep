cf2b4e0e0e3afaea40ec965753f956bce63bc79d
Guillaume Huysmans <ghuysmans99@gmail.com>
2021-11-27 17:57:56 +0100

    Naively prepend the database name (#7)

--- utils/dep_to_dot.sh
+++ utils/dep_to_dot.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 echo 'digraph {'
+(
+	if [ -z "$1" ]; then
+		cat
+	else
+		sed -Ee "s/([A-Za-z0-9_.]+)/$1.\1/g" \
+        -e 's/^[^.]+\.(TABLE|VIEW)/\1/'
+	fi
+) |
 sed -Ee 's/^TABLE ([^:]+)/\1 [shape=cylinder]\n\1/' \
     -e 's/^VIEW ([^:]+)/\1 [shape=house]\n\1/' |
 sed -Ee 's/^([A-Za-z0-9_.]+): (.*)/\1 -> {\2}/' -e 's/\./__/g'
