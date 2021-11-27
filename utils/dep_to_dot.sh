2596451aa641f870f2d6a53fb50f3a340187d335
Guillaume Huysmans <ghuysmans99@gmail.com>
2021-11-27 19:04:30 +0100

    Use labels

--- utils/dep_to_dot.sh
+++ utils/dep_to_dot.sh
@@ -11,5 +11,6 @@ echo 'digraph {'
 ) |
 sed -Ee 's/^TABLE ([^:]+)/\1 [shape=cylinder]\n\1/' \
     -e 's/^VIEW ([^:]+)/\1 [shape=house]\n\1/' |
-sed -Ee 's/^([A-Za-z0-9_.]+): (.*)/\1 -> {\2}/' -e 's/\./__/g'
+sed -Ee 's/^([A-Za-z0-9_]+)\.([A-Za-z0-9_]+) \[/\1__\2 [label="\1.\2", /' -e '/.*:.*/s/\./__/g' |
+sed -E 's/^([A-Za-z0-9_.]+): (.*)/\1 -> {\2}/'
 echo '}'
